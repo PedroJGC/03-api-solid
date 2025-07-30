8a60783ea5bb8095c1e20cfe62594fa96e455ff7
PedroJGC <pedro.juliogc02@gmail.com>
2025-07-30 18:51:26 -0300

    feat: loading environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    // biome-ignore lint/suspicious/noConsole: console required
+    console.log('🚀 HTTP Server Running!')
   })
