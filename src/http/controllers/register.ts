f48a8aa8aa471a54be17ee261d3bba4d41795375
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-13 17:37:38 -0300

    feat: implement user registration conflict handling with custom error class

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -21,9 +22,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     })
-    // biome-ignore lint/correctness/noUnusedVariables: ignore
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500) // TODO:fix me
   }
 
   return reply.status(201).send()
