5ffe8620347855d5075bed483df53ca736e184b2
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-15 18:18:09 -0300

    feat: enhance registration use case with password hashing and response structure

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,6 @@
 /** biome-ignore-all lint/style/noParameterProperties: ignore */
+
+import type { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import type { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
@@ -9,10 +11,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +31,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
