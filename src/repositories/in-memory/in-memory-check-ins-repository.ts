19e5fa88e04c5c102d296ea8f82e4a65146c4344
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-30 17:09:27 -0300

    feat: TDD and mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -8,6 +8,19 @@ import type { CheckInsRepository } from '../check-ins-repository'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  // biome-ignore lint/correctness/noUnusedFunctionParameters: ignore
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameData = this.items.find(
+      (checkIn) => checkIn.user_id === userId
+    )
+
+    if (!checkInOnSameData) {
+      return null
+    }
+
+    return checkInOnSameData
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
