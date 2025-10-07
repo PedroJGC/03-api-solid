0532af765c20047613a3ba4de16b35af022467d8
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-07 17:58:05 -0300

    feat: integrate JWT authentication and add profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 
 // biome-ignore lint/suspicious/useAwait: ignore
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
