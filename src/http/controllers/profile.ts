dc6e6db5c341641bc84de6cf7271c58945607a82
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-07 18:24:19 -0300

    feat: add Fastify JWT type definitions and log user ID in profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -3,5 +3,8 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify()
 
+  // biome-ignore lint/suspicious/noConsole: ignore
+  console.log(request.user.sub)
+
   return reply.status(200).send()
 }
