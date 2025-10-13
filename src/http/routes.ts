32773f2d4311d88bdd01b0a17a5821607ef9c133
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-13 17:14:03 -0300

    feat: profile controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,13 @@ import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 // biome-ignore lint/suspicious/useAwait: ignore
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.get('/me', profile)
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
