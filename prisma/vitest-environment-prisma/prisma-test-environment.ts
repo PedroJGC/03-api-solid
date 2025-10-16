200929851a438aabcf708fc4854d9ce4eaf1ad05
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-16 19:01:33 -0300

    fix: remove 'public' schema prefix from users, check_ins, and gyms tables in migration files

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,33 @@
 /** biome-ignore-all lint/suspicious/useAwait: ignore */
 /** biome-ignore-all lint/suspicious/noConsole: ignore */
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest/environments'
 
-export default (<Environment>{
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default {
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseUrl = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseUrl
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
@@ -14,4 +35,4 @@ export default (<Environment>{
       },
     }
   },
-})
+} as Environment
