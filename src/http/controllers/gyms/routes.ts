a5524c46ba9b09c83273f3570567255a326b5e04
PedroJGC <pedro.juliogc02@gmail.com>
2025-10-23 17:16:17 -0300

    feat: add nearby and search gym endpoints with validation

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import type { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { create } from './create'
+import { nearby } from './nearby'
+import { search } from './search'
 
 // biome-ignore lint/suspicious/useAwait: ignore
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
